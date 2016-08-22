1a62fbbd17688234151d10a3996549156e8de5dd
xin.qi <xin.qi@sim.com>
2016-08-22 13:26:50 +0800

    Thread_safe: Update test programmer

--- src/main.c
+++ src/main.c
@@ -3,31 +3,56 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 typedef struct
 {
-    thread_t        *worker_thread;
-    pthread_mutex_t worker_thread_lock;
-    bool            epilog_timer_created;
-    int         epilog_timer_id;
+	thread_t        *worker_thread;
+	pthread_mutex_t worker_thread_lock;
+	bool            epilog_timer_created;
+	int         epilog_timer_id;
 } bt_hc_cb_t;
 static bt_hc_cb_t hc_cb;
 
-static void hello_world(void *context)  
+static void hello_thread(void)
 {
 	while(1) {
-		printf("create pthread start \n");
+		printf("hello_world \n");
 		sleep(1);
-		printf("hello world \n");
+	}
+}
+static void can_thread(void)
+{
+	while(1) {
+		printf("can_world \n");
+		sleep(1);
+	}
+}
+
+
+static void hello_world(void *context)  
+{
+	int ret;
+	pthread_t thread[0];
+
+	printf("create pthread_create \n");
+	ret = pthread_create(&thread[0], NULL, (void *)hello_thread, NULL);
+	if (ret) {
+		printf("pthread_create error \n");
+		exit(1);
 	}
 }
 
 static void can_test(void *context)
 {
-	while(1) {
-		printf("create pthread start \n");
-		sleep(1);
-		printf("can test \n");
+	int ret;
+	pthread_t thread[0];
+
+	printf("create pthread_create \n");
+	ret = pthread_create(&thread[0], NULL, (void *)can_thread, NULL);
+	if (ret) {
+		printf("pthread_create error \n");
+		exit(1);
 	}
 }
 
@@ -48,10 +73,10 @@ int main(int argc, const char *argv[])
 	pthread_mutex_init(&hc_cb.worker_thread_lock, NULL);
 
 	if (hc_cb.worker_thread) {
-        printf("init has been called repeatedly without calling cleanup ?");
+		printf("init has been called repeatedly without calling cleanup ?");
 	}
 	hc_cb.worker_thread = thread_new("hello");
-  	printf("ret = %p \n", hc_cb.worker_thread);
+	printf("ret = %p \n", hc_cb.worker_thread);
 	if (!hc_cb.worker_thread) {
 		printf("unable to create worker thread \n");
 		return -1;
@@ -61,13 +86,9 @@ int main(int argc, const char *argv[])
 		thread_post(hc_cb.worker_thread, hello_world, NULL);
 
 	if (hc_cb.worker_thread)
-		//thread_post(hc_cb.worker_thread, can_test, NULL);
+		thread_post(hc_cb.worker_thread, can_test, NULL);
 
-	sleep(1);
-
-	thread_stop(hc_cb.worker_thread);
-
-	sleep(1);
+	sleep(10);
 
 	cleanup();
 
